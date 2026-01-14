bd7ce4dde624557bb509c10f5de67dcd5beb071c
mvntaha <syedamuntahapk@gmail.com>
2026-01-14 16:55:27 +0500

    configured APIs in the app

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,18 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electron', {
-    send: (channel, data) => ipcRenderer.send(channel, data),
-    on: (channel, callback) =>
-        ipcRenderer.on(channel, (_, ...args) => callback(...args))
+contextBridge.exposeInMainWorld('api', {
+    createProject: (name, description, due_date) =>
+        ipcRenderer.invoke('project:create', { name, description, due_date }),
+
+    addProjectDetail: (project_id, key, value, status, priority) =>
+        ipcRenderer.invoke('project:addDetail', { project_id, key, value, status, priority }),
+
+    getProjectWithDetails: (project_id) =>
+        ipcRenderer.invoke('project:getWithDetails', project_id),
+
+    updateProjectDetail: (project_detail_id, key, value, status, priority) =>
+        ipcRenderer.invoke('project:updateDetail', { project_detail_id, key, value, status, priority }),
+
+    deleteProject: (project_id) =>
+        ipcRenderer.invoke('project:delete', project_id),
 });
